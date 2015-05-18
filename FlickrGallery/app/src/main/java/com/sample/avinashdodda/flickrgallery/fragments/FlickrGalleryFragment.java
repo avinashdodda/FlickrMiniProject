e2569c0aaac076af7c8d48f8cd2b0408048a15f9
Avinash Dodda <avinash.dodda@gmail.com>
2015-05-18 02:12:23 -0400

    adding package for fragments

--- FlickrGallery/app/src/main/java/com/sample/avinashdodda/flickrgallery/fragments/FlickrGalleryFragment.java
+++ FlickrGallery/app/src/main/java/com/sample/avinashdodda/flickrgallery/fragments/FlickrGalleryFragment.java
@@ -1,10 +1,12 @@
-package com.sample.avinashdodda.flickrgallery;
+package com.sample.avinashdodda.flickrgallery.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.sample.avinashdodda.flickrgallery.R;
 
 /**
  * Created by Avinash Dodda on 5/16/15.
